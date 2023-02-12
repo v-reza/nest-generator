1306bc7c11c49bee56dc3d1f722120c80904b8c5
v-reza <mrezalf0@gmail.com>
2023-02-12 18:20:36 +0700

    vercel

--- src/main.ts
+++ src/main.ts
@@ -17,7 +17,12 @@ async function bootstrap() {
     .build()
 
   const document = SwaggerModule.createDocument(app, config)
-  SwaggerModule.setup('swagger', app, document)
+  SwaggerModule.setup('swagger', app, document, {
+    customCssUrl: 'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.min.css',
+    customSiteTitle: 'Backend Generator',
+    customfavIcon: 'https://avatars.githubusercontent.com/u/6936373?s=200&v=4',
+    customJs: 'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-bundle.min.js',
+  })
   const cors = { ...CorsConfig }
   app.enableCors(cors);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
